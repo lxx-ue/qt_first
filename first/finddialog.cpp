d61d5bc05423b5a3cf2a50442b2a0667cdab38dd
lxx-ue <71316369+lxx-ue@users.noreply.github.com>
2022-07-25 22:01:01 +0300

    add find dialog

---
+++ first/finddialog.cpp
@@ -0,0 +1,6 @@
+#include "finddialog.h"
+
+FindDialog::FindDialog()
+{
+
+}
---
+++ first/finddialog.h
@@ -0,0 +1,11 @@
+#ifndef FINDDIALOG_H
+#define FINDDIALOG_H
+
+
+class FindDialog
+{
+public:
+    FindDialog();
+};
+
+#endif // FINDDIALOG_H
